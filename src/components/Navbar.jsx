c4e0671ad6ec0f73f5549b43bb9a7f06f183ad7a
essteer <essteer@pm.me>
2024-08-27 14:23:00 +0800

    feat: Add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import logo from "../VANN_WEB_LOGO.avif";
+import "../styles/Navbar.css"
+
+const Navbar = () => {
+
+    return (
+        <div>
+            <nav className="navbar">
+                <a href="/">
+                    <img className="logo" src={logo} alt="VANN logo"></img>
+                </a>
+                <ul className="nav-links">
+                    <li>
+                        <a href="/shop">Shop</a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/vann_jewellery/" target="_blank">Instagram</a>
+                    </li>
+                    <li>
+                        <a href="/contact">Contact</a>
+                    </li>
+                    <li>
+                        <a href="/search">Search</a>
+                    </li>
+                    <li>
+                        <a href="/cart">Cart</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
