7a6229100a24aad207ab708d811b49c3f5bfbd4b
essteer <essteer@pm.me>
2024-08-27 15:20:20 +0800

    feat: Add scrolling banner below navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-import Footer from './components/Footer.jsx';
-import Navbar from "./components/Navbar.jsx"
+import "./App.css";
+import Banner from "./components/Banner.jsx";
+import Footer from "./components/Footer.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Marquee from "react-fast-marquee";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
+      <Marquee autoFill="true" speed={85} >
+        <Banner />
+      </Marquee>
+
       <Footer />
     </div>
   );
---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import "../styles/Banner.css"
+
+const Banner = () => {
+
+    return (
+        <div className="banner">
+            <h2>SHIPPING WORLDWIDE / INTERNATIONAL EXPRESS DELIVERY / FREE LOCAL DELIVERY / </h2>
+        </div>
+    )
+}
+
+export default Banner
