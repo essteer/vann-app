3cddf15e2062a182507e9ddc64d682f8c446a1c5
essteer <essteer@pm.me>
2024-08-27 16:33:38 +0800

    feat: Add initial main body component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Banner from "./components/Banner.jsx";
 import BaseLogo from "./components/BaseLogo.jsx";
 import Footer from "./components/Footer.jsx";
+import Main from "./components/Main.jsx"
 import Navbar from "./components/Navbar.jsx";
 import Marquee from "react-fast-marquee";
 
@@ -12,6 +13,8 @@ function App() {
       <Marquee autoFill="true" speed={85}>
         <Banner />
       </Marquee>
+
+      <Main />
 
       <Footer />
       <BaseLogo />
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import "../styles/Main.css"
+
+const Main = () => {
+
+    return (
+        <div className="main-container">
+           
+        </div>
+    )
+}
+
+export default Main
