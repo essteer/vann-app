ef07e9910ef79c86ab2385ab22a35a5643b94277
essteer <essteer@pm.me>
2024-08-27 16:48:37 +0800

    chore: Config react-router-dom and basic routes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import "./App.css";
 import Banner from "./components/Banner.jsx";
 import BaseLogo from "./components/BaseLogo.jsx";
 import Footer from "./components/Footer.jsx";
-import Main from "./components/Main.jsx"
+import Main from "./components/Main.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Marquee from "react-fast-marquee";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Marquee autoFill="true" speed={85}>
-        <Banner />
-      </Marquee>
 
-      <Main />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Marquee autoFill="true" speed={85}>
+                <Banner />
+              </Marquee>
+              <Main />
+            </div>
+          }
+        />
+      </Routes>
 
       <Footer />
       <BaseLogo />
