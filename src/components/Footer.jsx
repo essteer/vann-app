7d8ef5aba29c02099a0a66857809ffe313e2d5df
essteer <essteer@pm.me>
2024-08-28 18:25:12 +0800

    feat: Add onClick window.scrollTo event to Footer Link components to scroll to top of new pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,23 +9,23 @@ const Footer = () => {
                 <div className="column">
                     <h3>Client services</h3>
                     <ul>
-                        <li><Link to="/pages/delivery-and-return" >Delivery & returns</Link></li>
-                        <li><Link to="/pages/privacy-policy" >Privacy policy</Link></li>
-                        <li><Link to="/pages/contact" >Contact us</Link></li>
+                        <li><Link to="/pages/delivery-and-return" onClick={() => window.scrollTo(0, 0)} >Delivery & returns</Link></li>
+                        <li><Link to="/pages/privacy-policy" onClick={() => window.scrollTo(0, 0)} >Privacy policy</Link></li>
+                        <li><Link to="/pages/contact" onClick={() => window.scrollTo(0, 0)} >Contact us</Link></li>
                     </ul>
                 </div>
                 <div className="column">
                     <h3>Product information</h3>
                     <ul>
-                        <li><Link to="/pages/product-care" >Product care</Link></li>
-                        <li><Link to="/pages/product-specifications" >Product specifications</Link></li>
-                        <li><Link to="/pages/ring-size-chart" >Ring size chart</Link></li>
+                        <li><Link to="/pages/product-care" onClick={() => window.scrollTo(0, 0)} >Product care</Link></li>
+                        <li><Link to="/pages/product-specifications" onClick={() => window.scrollTo(0, 0)} >Product specifications</Link></li>
+                        <li><Link to="/pages/ring-size-chart" onClick={() => window.scrollTo(0, 0)} >Ring size chart</Link></li>
                     </ul>
                 </div>
                 <div className="column">
                     <h3>Company</h3>
                     <ul>
-                        <li><Link to="/pages/about" >About us</Link></li>
+                        <li><Link to="/pages/about" onClick={() => window.scrollTo(0, 0)} >About us</Link></li>
                         <li><a href="https://www.instagram.com/vann_jewellery/" target="_blank" >Press</a></li>
                     </ul>
                 </div>
