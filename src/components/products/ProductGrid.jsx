b96121c34b7969af64c41ed8b044f3aa02e4ebe5
essteer <essteer@pm.me>
2024-08-29 12:52:07 +0800

    refactor: Remove category check from ProductGrid

--- src/components/products/ProductGrid.jsx
+++ src/components/products/ProductGrid.jsx
@@ -1,20 +1,18 @@
 import ProductCard from './ProductCard';
 import '../../styles/ProductGrid.css';
 
-function ProductGrid({ category, products }) {
+function ProductGrid({ products }) {
 
     return (
         <div>
             <div className="product-grid">
                 {products.map((product) => (
-                    category === 'all' || category === product.category ? (
                     <ProductCard
-                        key={product.name}
-                        imageURI={product.image}
-                        name={product.name}
-                        price={product.price}
+                        key={product.productId}
+                        imageURI={product.productImage}
+                        name={product.productName}
+                        price={product.productPrice}
                     />
-                    ) : null 
                 ))}
             </div>
             <div className="spacing" ></div>
