f0610d11cc03862b4a36c39bf51862a66260459f
essteer <essteer@pm.me>
2024-09-01 17:37:07 +0800

    refactor: Remove hard-coded data and update imports for CSS

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,14 +5,6 @@ import ProductGrid from "./products/ProductGrid.jsx";
 
 const Main = () => {
 
-    // hard-coded product examples for development without live API
-    // const featuredProducts = [
-    //     { productId: 'f2994d85-ab6e-4fe6-b999-b8379dd34753', category: 'ear cuffs', productImage: '/files/HEART_EAR_CUFF_WG.jpg?v=1706532715', productName: 'HEART EAR CUFF', productPrice: 105 },
-    //     { productId: '7bf6e551-b6ae-4fdd-af91-cca91ee812e8', category: 'bracelets', productImage: '/products/Nova_Bracelet.jpg?v=1707045353', productName: 'NOVA BRACELET', productPrice: 365 },
-    //     { productId: '9c0d5e24-1e63-474f-8e30-2e5f3cf0efce', category: 'necklaces', productImage: '/files/Pearl_chocker_Heather.jpg?v=1712216804', productName: 'PEARL CHOKER NECKLACE', productPrice: 315 },
-    //     { productId: '591ccb5d-b598-4c6e-826f-a5d28cfce837', category: 'earrings', productImage: '/files/XL_Molten_F.jpg?v=1706969778&width=1200', productName: 'XL MOLTEN EARRINGS', productPrice: 575 },
-    // ];
-
     const api = 'http://localhost:9001/api/v1/products'
     const [featuredProducts, setFeaturedProducts] = useState([]);
 
