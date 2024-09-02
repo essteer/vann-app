e461f326eadf306eb0bfd8259299fe73535f1c4d
essteer <essteer@pm.me>
2024-09-02 10:43:13 +0800

    chore: Add subtitles to page content

--- src/components/pages/PrivacyPolicy.jsx
+++ src/components/pages/PrivacyPolicy.jsx
@@ -3,13 +3,17 @@ import Page from "../Page";
 const PrivacyPolicy = () => {
 
     const title = 'Privacy policy';
+    const subtitles = [
+        'Personal data',
+        'Third parties'
+    ]
     const text = [
         'We will use the personal information you provide to us through our website to supply products to you; to process your payment for products; and to update you with our latest news if you subscribe to our mailing list.',
         'We will only provide personal information to third parties as the law requires or permits us to do so.'
     ];
 
     return (
-        <Page title={title} text={text} />
+        <Page title={title} subtitles={subtitles} text={text} />
     )
 }
 
--- src/components/pages/ProductSpecifications.jsx
+++ src/components/pages/ProductSpecifications.jsx
@@ -3,13 +3,17 @@ import Page from "../Page";
 const ProductSpecifications = () => {
 
     const title = 'Product specifications';
+    const subtitles = [
+        'Handmade',
+        'Colours'
+    ]
     const text = [
         'All of our products are handmade — naturally, forms may vary slightly from product to product and images on this website are for illustrative purposes only. Sizes, weights and measurements indicated on our website are subject to a 5% tolerance range.',
         'Although we make every effort to depict colours accurately, we cannot guarantee that images rendered on devices reflect the true colour of the products.',
     ];
 
     return (
-        <Page title={title} text={text} />
+        <Page title={title} subtitles={subtitles} text={text} />
     )
 }
 
